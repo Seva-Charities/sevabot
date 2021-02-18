00c8c9b80eb6270c3e223e5b9cb6e5f55e8f244d
neil-sriv <neil.srivastava1@gmail.com>
2021-02-17 21:48:38 -0600

    fourth commit for heroku

--- app.py
+++ app.py
@@ -1,66 +1,25 @@
-import os
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request
 import dotenv
-from slack_bolt import App
-import re
 import logging
 
-# Initializes your app with your bot token and signing secret
-dotenv.load_dotenv(dotenv_path='.env')
+from slack_bolt import App
+
+dotenv.load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
-
-app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-)
-
-# Listens to incoming messages that contain "hello"
+app = App()
 
 
-@app.message("hell")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
+@app.command("/hello-bolt-python-heroku")
+def hello(body, ack):
+    user_id = body["user_id"]
+    ack(f"Hi <@{user_id}>!")
 
 
-@app.message(re.compile("(seva|Seva)"))
-def react_seva(message, say):
-    print(message)
-    # say(
-    #     blocks=[
-    #         {
-    #             "type": "section",
-    #             "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-    #             "accessory": {
-    #                 "type": "button",
-    #                 "text": {"type": "plain_text", "text": "Click Me"},
-    #                 "action_id": "button_click"
-    #             }
-    #         }
-    #     ],
-    #     text=f"Hey there <@{message['user']}>!"
-    # )
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
 
 
-@ app.action("button_click")
-def action_button_click(body, ack, say):
-    # Acknowledge the action
-    ack()
-    say(f"<@{body['user']['id']}> clicked the button")
-
-
-# Start your app
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
