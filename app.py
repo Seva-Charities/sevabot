a207f530d30f500fa5fe9eeb349141c52e10d639
neil-sriv <neil.srivastava1@gmail.com>
2021-02-17 21:58:41 -0600

    hello test

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def hello(body, ack):
     user_id = body["user_id"]
     ack(f"Hi <@{user_id}>!")
 
+# Listens to incoming messages that contain "hello"
+
+
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
 
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
