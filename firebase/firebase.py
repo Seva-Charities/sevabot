0aaeb0b7904a6c003ae49d9708998c90f5f24b8a
neil-sriv <neil.srivastava1@gmail.com>
2021-05-19 16:59:27 -0500

    heroku debug

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
+import sys
+
+print(os.listdir())
+print(sys.path)
 
 cred = credentials.Certificate("/Users/neil/cs/slack-bot/sevabot/firebase/creds.json")
 firebase_admin.initialize_app(cred)
